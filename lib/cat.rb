9efc1f27508211bcaec926122cd76a84d9321822
Harrf Akbar <harrf_akbar@yahoo.com>
2021-01-17 18:51:17 -0800

    commit tree 3ae5af3e02b54ff084ff1d7434f344ce0a5b1847

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
 class Cat
   # code goes here
+
+  attr_accessor :mood, :owner 
+  attr_reader :name
+
+  @@cats = []
+
+  def initialize(name)
+    @name = name 
+    @mood = "nervous"
+    @@cats << self
+  end
+
+  def self.all
+    @@cats 
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
+
+
 class Owner
   # code goes here
+
+  attr_reader :name, :species 
+
+  @@owners = []
+
+  def initialize(name)
+    @name = name 
+    @species = species
+    @@owners << self 
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def self.all
+    @@owners 
+  end
+
+  def self.count
+    @@owners.length 
+  end
+
+  def self.reset_all
+    @@owners.clear 
+  end
 end
